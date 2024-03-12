8e0a5d4e765ae1df071a1f04f654532f83b8df63
liansun96 <toetetaung3296@gmail.com>
2024-03-12 16:01:14 +0630

    revision

--- Promises/Js_Fetch.js
+++ Promises/Js_Fetch.js
@@ -1,24 +1,42 @@
 const url =
-  "https://api.themoviedb.org/3/discover/movie?api_key=7fe0fbe72550cf27a1a5a738cabda3db&language=en-US&page=430";
+  "https://api.themoviedb.org/3/discover/movie?api_key=7fe0fbe72550cf27a1a5a738cabda3db&language=en-US&page=4";
 // const url = "https://fakestoreapi.com/products/1";
 
 const options = {
   method: "GET",
 };
 
-function fetchData(url){
-  return new Promise((resolve, reject) => {
-    fetch(url).then((res)=>{
-      if(!res.ok){
-        throw new Error(`Http error status: ${res.status}`)
-      }
-      return res.json();
-    }).then((data)=>{
-      resolve(data)
-    }).catch((err)=>{
-      console.error(err);
-    })
-  })
+// function fetchData(url) {
+//   return new Promise((resolve, reject) => {
+//     fetch(url)
+//       .then((res) => {
+//         if (!res.ok) {
+//           throw new Error(`Http error status:${res.status}`);
+//         }
+//         return res.json();
+//       })
+//       .then((data) => {
+//         resolve(data);
+//       })
+//       .catch((err) => {
+//         console.error(err);
+//       });
+//   });
+// }
+
+async function fetchData(url) {
+  try {
+    const res = await fetch(url);
+    if (!res.ok) {
+      throw new Error(`Http error status ${res.status}`);
+    }
+    const data = res.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-fetchData(url).then((data)=>console.log(data)).catch((err)=>console.error(err))
+fetchData(url)
+  .then((data) => console.log(data))
+  .catch((err) => console.error(err));
--- Promises/Promises.js
+++ Promises/Promises.js
@@ -1,4 +1,4 @@
-console.log('start');
+console.log("start");
 
 // const bob = new Promise((resolve , reject)=>{
 //     setTimeout(()=>{
@@ -15,32 +15,25 @@ console.log('start');
 //     console.log(err);
 // })
 
-
-
 const url = "https://fakestoreapi.com/products/15";
 
-function fetchData(url){
-    return new Promise((resolve, reject) => {
-        fetch(url).then((res) => {
-            if(!res.ok){
-                throw new Error(`Http error status:${res.status}`)
-            }
-            return res.json();
-        }).then((data)=> {
-            resolve(data)
-        }).catch((err)=>{
-            console.error(err);
-        })
+function fetchData(url) {
+  return new Promise((resolve, reject) => {
+    fetch(url).then((res)=>{
+        if(!res.ok){
+            throw new Error(`Http error status:${res.status}`)
+        }
+        return res.json();
+    }).then((data)=>{
+        resolve(data)
+    }).catch((err)=>{
+        console.error(err);
     })
+  });
 }
 
-fetchData(url).then((data)=> {
-    console.log(data);
-}).catch((err)=>{
-    console.log(err);
-})
+fetchData(url).then((data)=>console.log(data)).catch((err)=>console.error(err))
 
-setTimeout(()=>console.log('setTimeout'),0)
+setTimeout(() => console.log("setTimeout"), 0);
 
-console.log('end');
-
+console.log("end");
